6d6686525e8b3d046d30ba1af83a3fbe34c483f7
KevinFloris20 <75303163+KevinFloris20@users.noreply.github.com>
2023-12-27 00:26:45 -0500

    currently working on rest req to gcp

--- public/script.js
+++ public/script.js
@@ -1,24 +1,29 @@
 document.addEventListener('DOMContentLoaded', function() {
     let formData;  // Store the fetched form data
-
+    let fieldValues = {};  // Store current field values
+    
     function clearFormFields() {
         const existingFields = document.querySelectorAll('#invoice-form .dynamic-input');
-        existingFields.forEach(field => field.remove());
+        existingFields.forEach(field => {
+            // Save current field value before removing
+            fieldValues[field.name] = field.value;
+            field.remove();
+        });
     }
-
+    
     // Function to create and position form fields
     function createFormFields(fields, scaleFactor) {
         const form = document.getElementById('invoice-form');
         clearFormFields();
-        const adjustmentFactor = 0.003; //adjustment
-
+        const adjustmentFactor = 0.003; // Adjustment
+    
         fields.forEach(field => {
             // Scaling positions and dimensions
             const scaledX = field.x * scaleFactor;
             const scaledY = field.y * scaleFactor - form.offsetHeight * adjustmentFactor;
             const scaledWidth = field.w * scaleFactor;
             const scaledHeight = field.h * scaleFactor;
-
+    
             // Create a div to hold the input field
             const fieldDiv = document.createElement('div');
             fieldDiv.style.position = 'absolute';
@@ -26,7 +31,7 @@ document.addEventListener('DOMContentLoaded', function() {
             fieldDiv.style.top = `${scaledY}px`;
             fieldDiv.style.width = `${scaledWidth}px`;
             fieldDiv.style.height = `${scaledHeight}px`;
-
+    
             // Create the input field
             const input = document.createElement('input');
             input.classList.add('dynamic-input');
@@ -37,7 +42,7 @@ document.addEventListener('DOMContentLoaded', function() {
             input.style.border = 'none'; // Optional: remove border
             input.style.padding = '0'; // Optional: remove padding
             input.style.margin = '0'; // Optional: remove margin
-
+    
             if((String(field.id) != 'A') && (String(field.id).includes('A'))){
                 input.style.textAlign = 'center';
             }
@@ -45,23 +50,29 @@ document.addEventListener('DOMContentLoaded', function() {
                 input.style.textAlign = 'right';
             }
             if(String(field.id) == 'C'){
-                input.style.textAlign = 'center'
+                input.style.textAlign = 'center';
             }
             if(String(field.id) == 'D'){
-                input.style.textAlign = 'center'
+                input.style.textAlign = 'center';
             }
+    
             // Set font size based on the height of the field
             const fontSize = scaledHeight * 0.9; // Adjust the factor as needed
             input.style.fontSize = `${fontSize}px`;
-
+    
+            // Repopulate field values after creating new fields
+            if (fieldValues[field.id] !== undefined) {
+                input.value = fieldValues[field.id];
+            }
+    
             // Append the input to the div
             fieldDiv.appendChild(input);
-
+    
             // Append the div to the form
             form.appendChild(fieldDiv);
         });
     }
-
+    
     // Function to handle window resize
     function onResize() {
         const form = document.getElementById('invoice-form');
@@ -69,17 +80,37 @@ document.addEventListener('DOMContentLoaded', function() {
         const scaleFactor = pngWidth / formData.pdfWidth;
         createFormFields(formData.fields, scaleFactor);
     }
-
+    
     // Fetch form data and create fields
     fetch('/formdata')
-    .then(response => response.json())
-    .then(data => {
-        formData = data; // Store the data for use in resize event
-        onResize(); // Create fields initially
-    })
-    .catch(error => console.error('Error fetching data:', error));
-
+        .then(response => response.json())
+        .then(data => {
+            formData = data; // Store the data for use in resize event
+            onResize(); // Create fields initially
+        })
+        .catch(error => console.error('Error fetching data:', error));
+    
     // Add resize event listener
     window.addEventListener('resize', onResize);
+    
+
+
+
+
+    // this is the stuff for the menu
+    document.getElementById('menuNewInv').addEventListener('click', function() {
+        showSection('inv');
+    });
+    document.getElementById('menuSearch').addEventListener('click', function() {
+        showSection('searchSection');
+    });
+    function showSection(sectionId) {
+        var sections = document.getElementsByClassName('ui segment');
+        for (var i = 0; i < sections.length; i++) {
+            sections[i].style.display = 'none';
+        }
+            document.getElementById(sectionId).style.display = '';
+    }
+    
 });
 
