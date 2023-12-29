02c1e1a4a4075e9c268993132c22c630ce07eb14
KevinFloris20 <75303163+KevinFloris20@users.noreply.github.com>
2023-12-29 13:48:10 -0500

    added the send new data app file

--- server/routes.js
+++ server/routes.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 //get the pdfdata
-const { extractFormFields } = require('./extractPDFData');
+const { extractFormFields } = require('./models/extractPDFData');
 router.get('/formdata', async (req, res) => {
     try {
         const pdfPath = 'FCRInvoiceTemplateNull.pdf';
@@ -13,6 +13,27 @@ router.get('/formdata', async (req, res) => {
         res.status(500).send('Error processing PDF');
     }
 });
+
+//just run the sendObjToDB.js file
+const { interactWithFirestore } = require('./models/sendObjToDB');
+var sampledata = {
+    invoiceid: null,
+    wholeInvoice: {
+      A: "any string value",
+      B: "any string value",
+      C: "any string value",
+      D: "any string value",
+      invoicemap: {
+        "key1": "value1",
+        "key2": "value2",
+        "key3": "value3",
+        "key4": "value4",
+        "key5": "value5",
+      }
+    }
+};
+
+interactWithFirestore("writeData", sampledata);
 
 //export
 module.exports = router;
